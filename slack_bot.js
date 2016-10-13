e0edd3fd85f767f5c0c2d16b5e89b91efec4cc58
Jun Kai Lim <junkaiii@gmail.com>
2016-10-14 01:42:28 +0800

    stable

--- slack_bot.js
+++ slack_bot.js
@@ -7,15 +7,18 @@ var Botkit = require('botkit');
 var os = require('os');
 var axios = require('axios');
 
+var current_user;
+
 
 var mongoStorage = require('botkit-storage-mongo')({
-  mongoUri: 'mongodb://<dbuser>:<dbpassword>@ds057066.mlab.com:57066/slackbot'
+  mongoUri: 'mongodb://junkaiii:4480866l@ds057066.mlab.com:57066/slackbot'
 });
 
 var controller = Botkit.slackbot({
-  logLevel: 1, // 1 to 7
+  // logLevel: 1, // 1 to 7
   stats_optout: true, // set true for privacy.
-  storage: mongoStorage
+  // storage: mongoStorage,
+  // json_file_store: 'path_to_json_database'
 });
 
 var bot = controller.spawn({
@@ -23,48 +26,11 @@ var bot = controller.spawn({
     // token: process.env.token
 }).startRTM();
 
-
-// controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
-//
-//     bot.api.reactions.add({
-//         timestamp: message.ts,
-//         channel: message.channel,
-//         name: 'robot_face',
-//     }, function(err, res) {
-//         if (err) {
-//             bot.botkit.log('Failed to add emoji reaction :(', err);
-//         }
-//     });
-//
-//
-//     controller.storage.users.get(message.user, function(err, user) {
-//         if (user && user.name) {
-//             bot.reply(message, 'Hello ' + user.name + '!!');
-//         } else {
-//             bot.reply(message, 'Hello.');
-//         }
-//     });
-// });
-//
-// controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
-//     var name = message.match[1];
-//     controller.storage.users.get(message.user, function(err, user) {
-//         if (!user) {
-//             user = {
-//                 id: message.user,
-//             };
-//         }
-//         user.name = name;
-//         controller.storage.users.save(user, function(err, id) {
-//             bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-//         });
-//     });
-// });
-
 controller.hears(['find me (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
 
   var ingredient = message.match[1];
   var recipes = [];
+  var ids = [];
 
   axios.get('http://food2fork.com/api/search?key=d3799f6871828e8da7fce781da27fb8f&q=' + ingredient)
     .then(function(response) {
@@ -75,14 +41,27 @@ controller.hears(['find me (.*)'], 'direct_message,direct_mention,mention', func
         }
         bot.reply(message, (i + 1) + '. ' + response.data.recipes[i].title);
         recipes.push(response.data.recipes[i].title);
+        ids.push(response.data.recipes[i].recipe_id);
       }
       bot.startConversation(message, function(err, convo) {
         convo.ask('Which would you like to have?', function(response, convo) {
+          chosen_recipe = ids[response.text];
           for (var i = 0; i < recipes.length; i++) {
             if (response.text == i + 1) {
               convo.ask('You have chosen: ' + recipes[i] + ' is that correct?', function(response, convo) {
                 if (response.text == 'yes') {
-                  convo.say('Great!');
+                  convo.say('Great, you will need: ');
+                  axios.get('http://food2fork.com/api/get?key=d3799f6871828e8da7fce781da27fb8f&rId=' + chosen_recipe)
+                    .then(function(response) {
+                      // response = response;
+                      // console.log(response.data.recipe.ingredients);
+                      for (var i = 0; i < response.data.recipe.ingredients.length; i++) {
+                        bot.reply(message, response.data.recipe.ingredients[i]);
+                      }
+                    })
+                    .catch(function(error) {
+                      console.log(error);
+                    });
                 } else if (response.text == 'no') {
                   convo.say('Oh No!');
                 }
@@ -97,147 +76,97 @@ controller.hears(['find me (.*)'], 'direct_message,direct_mention,mention', func
     .catch(function(error) {
       bot.reply(message, 'Sorry, I did not find any recipes with ' + ingredient + '.');
     });
-
-
-
-  // controller.storage.users.get(message.user, function(err, user) {
-  //     if (!user) {
-  //         user = {
-  //             id: message.user,
-  //         };
-  //     }
-  //     user.name = name;
-  //     controller.storage.users.save(user, function(err, id) {
-  //         // bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-  //         bot.reply(message, message.match);
-  //
-  //     });
-  // });
 });
 
-// controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
-//
-//     controller.storage.users.get(message.user, function(err, user) {
-//         if (user && user.name) {
-//             bot.reply(message, 'Your name is ' + user.name);
-//         } else {
-//             bot.startConversation(message, function(err, convo) {
-//                 if (!err) {
-//                     convo.say('I do not know your name yet!');
-//                     convo.ask('What should I call you?', function(response, convo) {
-//                         convo.ask('You want me to call you `' + response.text + '`?', [
-//                             {
-//                                 pattern: 'yes',
-//                                 callback: function(response, convo) {
-//                                     // since no further messages are queued after this,
-//                                     // the conversation will end naturally with status == 'completed'
-//                                     convo.next();
-//                                 }
-//                             },
-//                             {
-//                                 pattern: 'no',
-//                                 callback: function(response, convo) {
-//                                     // stop the conversation. this will cause it to end with status == 'stopped'
-//                                     convo.stop();
-//                                 }
-//                             },
-//                             {
-//                                 default: true,
-//                                 callback: function(response, convo) {
-//                                     convo.repeat();
-//                                     convo.next();
-//                                 }
-//                             }
-//                         ]);
-//
-//                         convo.next();
-//
-//                     }, {'key': 'nickname'}); // store the results in a field called nickname
-//
-//                     convo.on('end', function(convo) {
-//                         if (convo.status == 'completed') {
-//                             bot.reply(message, 'OK! I will update my dossier...');
-//
-//                             controller.storage.users.get(message.user, function(err, user) {
-//                                 if (!user) {
-//                                     user = {
-//                                         id: message.user,
-//                                     };
-//                                 }
-//                                 user.name = convo.extractResponse('nickname');
-//                                 controller.storage.users.save(user, function(err, id) {
-//                                     bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-//                                 });
-//                             });
-//
-//
-//
-//                         } else {
-//                             // this happens if the conversation ended prematurely for some reason
-//                             bot.reply(message, 'OK, nevermind!');
-//                         }
-//                     });
-//                 }
-//             });
-//         }
-//     });
-// });
-//
-//
-// controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
-//
-//     bot.startConversation(message, function(err, convo) {
-//
-//         convo.ask('Are you sure you want me to shutdown?', [
-//             {
-//                 pattern: bot.utterances.yes,
-//                 callback: function(response, convo) {
-//                     convo.say('Bye!');
-//                     convo.next();
-//                     setTimeout(function() {
-//                         process.exit();
-//                     }, 3000);
-//                 }
-//             },
-//         {
-//             pattern: bot.utterances.no,
-//             default: true,
-//             callback: function(response, convo) {
-//                 convo.say('*Phew!*');
-//                 convo.next();
-//             }
-//         }
-//         ]);
-//     });
-// });
-//
-//
-// controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
-//     'direct_message,direct_mention,mention', function(bot, message) {
-//
-//         var hostname = os.hostname();
-//         var uptime = formatUptime(process.uptime());
-//
-//         bot.reply(message,
-//             ':robot_face: I am a bot named <@' + bot.identity.name +
-//              '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-//
-//     });
-//
-// function formatUptime(uptime) {
-//     var unit = 'second';
-//     if (uptime > 60) {
-//         uptime = uptime / 60;
-//         unit = 'minute';
-//     }
-//     if (uptime > 60) {
-//         uptime = uptime / 60;
-//         unit = 'hour';
-//     }
-//     if (uptime != 1) {
-//         unit = unit + 's';
-//     }
-//
-//     uptime = uptime + ' ' + unit;
-//     return uptime;
-// }
+controller.hears(['call me (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
+  var name = message.match[1];
+  controller.storage.users.get(message.user, function(err, user) {
+    if (!user) {
+      user = {
+        id: message.user,
+        ingredients: [{
+          name: "mayonnaise",
+          quantity: 0.25,
+          unit: "cup"
+        }]
+      };
+    }
+    user.name = name;
+    controller.storage.users.save(user, function(err, id) {
+      bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+    });
+  });
+});
+
+controller.hears(['what ingredients'], 'direct_message,direct_mention,mention', function(bot, message) {
+  controller.storage.users.get(message.user, function(err, user) {
+    if (!user) {
+      user = {
+        id: message.user,
+        ingredients: [{
+          name: "mayonnaise",
+          quantity: 0.25,
+          unit: "tub"
+        },{
+          name: "jam",
+          quantity: 1,
+          unit: "jar"
+        }]
+      };
+    }
+    controller.storage.users.save(user, function(err, id) {
+      for (var i = 0; i < user.ingredients.length; i++) {
+        bot.reply(message, 'You have: ' + user.ingredients[i].name);
+      }
+    });
+  });
+});
+
+
+var ingredient_to_add = {};
+
+askName = function(response, convo) {
+  convo.ask("What is the ingredient you want to add?", function(response, convo) {
+    convo.say("Awesome.");
+    console.log(response);
+    ingredient_to_add.name = response.text;
+    askQuantity(response, convo);
+    convo.next();
+  });
+};
+askQuantity = function(response, convo) {
+  convo.ask("How many did you buy?", function(response, convo) {
+    convo.say("Ok.");
+    ingredient_to_add.quantity = response.text;
+    askUnit(response, convo);
+    convo.next();
+  });
+};
+askUnit = function(response, convo) {
+  convo.ask("Whats the unit of purchase?", function(response, convo) {
+    convo.say("Ok! Goodbye.");
+    ingredient_to_add.unit = response.text;
+    console.log(current_user);
+    controller.storage.users.get(current_user.id, function(err, user) {
+      current_user.ingredients.push(ingredient_to_add);
+      controller.storage.users.save(current_user, function(err, id) {
+        console.log(current_user);
+        convo.say('Ingredient added!');
+      });
+    });
+    convo.next();
+  });
+};
+
+controller.hears(['add ingredient'], 'direct_message,direct_mention,mention', function(bot, message) {
+  controller.storage.users.get(message.user, function(err, user) {
+    current_user = user;
+  });
+  bot.startConversation(message, askName);
+});
+
+controller.hears(['test'], 'direct_message,direct_mention,mention', function(bot, message) {
+  controller.storage.users.get(message.user, function(err, user) {
+    console.log(user);
+  });
+});
